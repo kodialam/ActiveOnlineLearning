0076afe2206029276a9780590cee9857988fa5f8
Rohan Kodialam <rkodialam@gmail.com>
2020-01-24 00:18:19 -0500

    Moved graphing utilities to separate file.

--- driver.py
+++ driver.py
@@ -1,5 +1,6 @@
 from DatasetGenerator import gp_datagen
 from OnlineClassifiers import train_al_perceptron
+from GraphingUtils import plot_points_and_separators
 from ALRules import *
 
 import matplotlib.pyplot as plt
@@ -51,32 +52,4 @@ for g in np.linspace(1, 20, 4):
         clfs['lm-sampling {:0.2f}'.format(g)] = clf_lm
 
 
-plt.scatter(
-    np.stack(data_test[:, 0])[:, 0],
-    np.stack(data_test[:, 0])[:, 1],
-    c=data_test[:, 1].astype(int)
-)
-ax = plt.gca()
-xlim = ax.get_xlim()
-ylim = ax.get_ylim()
-
-xx = np.linspace(xlim[0], xlim[1], 30)
-yy = np.linspace(ylim[0], ylim[1], 30)
-YY, XX = np.meshgrid(yy, xx)
-xy = np.vstack([XX.ravel(), YY.ravel()]).T
-
-contours = {}
-contour_handles = []
-for c in clfs:
-    contours[c] = clfs[c].decision_function(xy).reshape(XX.shape)
-    contour = ax.contour(
-        XX, YY,
-        contours[c],
-        levels=[0], alpha=0.5,
-        linestyles=['--'],
-        colors=[np.random.rand(3,)]
-    )
-    contour_handles.append(contour.legend_elements()[0][0])
-ax.legend(contour_handles, list(clfs.keys()))
-
-plt.show()
+plot_points_and_separators(data_test, clfs)
